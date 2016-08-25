2dd89039281ee51516afd824dc580ca2719ebf27
gutaewoo <gutaewoo@naver.com>
2016-08-26 08:49:52 +0900

    Update Syndra.cs

--- OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Champions/Syndra.cs
+++ OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Champions/Syndra.cs
@@ -249,7 +249,7 @@ namespace OneKeyToWin_AIO_Sebby.Champions
                 if (t.IsValidTarget())
                 {
                     if (Program.Combo && Player.Mana > RMANA + QMANA + WMANA)
-                        CatchW(t);
+                        ;
                     else if (Program.Farm && Config.Item("harrasW", false).GetValue<bool>() && Config.Item("harras" + t.ChampionName).GetValue<bool>() 
                         && Player.ManaPercent > Config.Item("QHarassMana", true).GetValue<Slider>().Value && OktwCommon.CanHarras())
                     {
